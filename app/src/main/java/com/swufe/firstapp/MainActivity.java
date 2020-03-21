d9a26679c652ecf0a55e3403dab41e3b4091463f
tomcatxxy <13659076313@163.com>
2020-03-21 18:03:51 +0800

    convertTemperature1.0

--- app/src/main/java/com/swufe/firstapp/MainActivity.java
+++ app/src/main/java/com/swufe/firstapp/MainActivity.java
@@ -6,20 +6,40 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.ButtonBarLayout;
 import androidx.appcompat.widget.Toolbar;
 
+import android.text.Editable;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //add something
+        TextView out=(TextView)findViewById(R.id.showText);
+        EditText in=(EditText)findViewById(R.id.inputText);
+        Button conv=(Button)findViewById(R.id.convert);
+        conv.setOnClickListener(this);
 
+    }
+    public void onClick(View v){
+        TextView out=(TextView)findViewById(R.id.showText);
+        EditText in=(EditText)findViewById(R.id.inputText);
+        try{
+            double tem=Double.valueOf(in.getText().toString());
+            out.setText("result:"+(tem*1.8+32));
+        }
+        catch (Exception e){
+            out.setText("result:error!!!please try again!");
+        }
     }
 
 
