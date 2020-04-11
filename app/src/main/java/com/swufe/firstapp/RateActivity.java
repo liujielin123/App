b4480c49a7eca8104fa746bfcddcdc6d4c9ed00b
tomcatxxy <13659076313@163.com>
2020-04-11 10:50:27 +0800

    rate2.0

--- app/src/main/java/com/swufe/firstapp/RateActivity.java
+++ app/src/main/java/com/swufe/firstapp/RateActivity.java
@@ -2,7 +2,9 @@ package com.swufe.firstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -12,6 +14,10 @@ public class RateActivity extends AppCompatActivity {
 
     EditText rmb;
     TextView show;
+    public final String Tag="RateActivity";
+    private double dollarRate=1/6.7;
+    private double euroRate=1/11.0;
+    private double wonRate=500.0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,15 +34,45 @@ public class RateActivity extends AppCompatActivity {
             String r=rmb.getText().toString();
             double rmb=Double.valueOf(r);
             if (btn.getId()==R.id.btn_dollar){
-                show.setText(String.format("%.2f",rmb/6.7));
+                show.setText(String.format("%.2f",rmb*dollarRate));
             }else if(btn.getId()==R.id.btn_euro){
-                show.setText(String.format("%.2f",rmb/11));
+                show.setText(String.format("%.2f",rmb*euroRate));
             }else {
-                show.setText(String.format("%.2f",rmb*500));
+                show.setText(String.format("%.2f",rmb*wonRate));
             }
         }
         catch (Exception e){
             Toast.makeText(this,"Please input your money!",Toast.LENGTH_SHORT).show();
         }
     }
+
+    public void openOne(View btn){
+        Log.i(Tag,"openOne");
+        Intent config=new Intent(this,ConfigActivity.class);
+        config.putExtra("dollar_rate_key",dollarRate);
+        config.putExtra("euro_rate_key",euroRate);
+        config.putExtra("won_rate_key",wonRate);
+        Log.i(Tag,"openOne:dollarRate"+dollarRate);
+        Log.i(Tag,"openOne:euroRate"+euroRate);
+        Log.i(Tag,"openOne:wonRate"+wonRate);
+
+        //startActivity(config);
+        startActivityForResult(config,1);
+    }
+
+    protected void onActivityResult(int requestCode,int resultCode,Intent data){
+        if(requestCode==1&&resultCode==2){
+            Bundle bdl=data.getExtras();
+            dollarRate=bdl.getDouble("key_dollar",0.0);
+            euroRate=bdl.getDouble("key_euro",0.0);
+            wonRate=bdl.getDouble("key_won",0.0);
+
+            Log.i(Tag,"onActivityResult:dollarRate="+dollarRate);
+            Log.i(Tag,"onActivityResult:euroRate="+euroRate);
+            Log.i(Tag,"onActivityResult:wonRate="+wonRate);
+        }
+        super.onActivityResult(requestCode,resultCode,data);
+    }
+
+
 }
