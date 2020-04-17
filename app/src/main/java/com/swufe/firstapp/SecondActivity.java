ed9833ab1eda45e479f2c92242c75d3a975c2ff1
tomcatxxy <13659076313@163.com>
2020-04-17 21:47:43 +0800

    score3.1

--- app/src/main/java/com/swufe/firstapp/SecondActivity.java
+++ app/src/main/java/com/swufe/firstapp/SecondActivity.java
@@ -1,17 +1,18 @@
 package com.swufe.firstapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
 
     TextView scoreA;
     TextView scoreB;
+    private final String Tag="SecondActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +20,28 @@ public class SecondActivity extends AppCompatActivity {
 
         scoreA=findViewById(R.id.scoreA);
         scoreB=findViewById(R.id.scoreB);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        Log.i(Tag,"onSaveInstanceState:");
+
+        //保存横屏或竖屏状态的数据
+        outState.putString("teamA_score",scoreA.getText().toString());
+        outState.putString("teamB_score",scoreB.getText().toString());
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        Log.i(Tag,"onRestoreInstanceState：");
+
+        //还原onSaveInstanceState保存的数据
+        scoreA.setText(savedInstanceState.getString("teamA_score"));
+        scoreB.setText(savedInstanceState.getString("teamB_score"));
     }
 
     public void btnAddA1(View v){ showScore(1,scoreA); }
